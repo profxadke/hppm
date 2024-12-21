edac1da8eeda0120c86407455b305baaa52532f3
Prof. - Xadk3! <xadkeg@duck.com>
2024-12-21 19:35:59 +0545

    Another commit, after a few dependencies.

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 
-import os
+import os, typer
 from subprocess import Popen, PIPE
+from anyio import create_task_group
 from httpx import AsyncClient
 
 
-async def main():
+app = typer.Typer()
+
+
+async def fetch_data(url: str) -> None:
     async with AsyncClient() as client:
-        response = await client.get('http://repo.zeenoverse.io/index.json')
-        resp = response.json()
-        print(resp)
+        print(f"[*] Fetching: {url}")
+        response = await client.get(url)
+        print(f"[+] Response from {url}: {response.status_code}\t{response.json()}")
+        return response.json()  # Returning the response as JSON
+
+
+async def fetch_repos() -> None:
+    repos = [
+        'http://repo.zeenoverse.io/index.json'
+    ]
+
+    async with create_task_group() as tg:
+        # Start each fetch_data task concurrently using task group
+        for url in repos:
+            tg.start_soon(fetch_data, url)
+
+    print('[X] All tasks finished!')
+
+
+@app.command()
+def main():
+    __import__('anyio').run(fetch_repos, backend='trio')
 
 
 if __name__ == '__main__':
-    __import__('uvloop').run(main())
+    app()
